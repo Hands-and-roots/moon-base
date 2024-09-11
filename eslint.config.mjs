017112f018fd96f1c95e3560432fabda3b7db94e
Hefazi <smohe1991@gmail.com>
2024-09-11 15:59:09 +0330

    Updated lint

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "**/.DS_Store",
+        "**/.cache/",
+        "**/.next/**/*",
+        "**/node_modules/",
+        "**/build/",
+        "**/coverage/",
+        "**/lib/",
+        "**/*.d.ts",
+        "**/*.min.js",
+        "**/*.map",
+        "**/*.snap",
+        "**/pnpm-lock.yaml",
+        "**/package-lock.json",
+        "**/yarn.lock",
+    ],
+}, ...compat.extends("moon", "moon/node", "moon/react"), {
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: "tsconfig.eslint.json",
+            tsconfigRootDir: __dirname,
+        },
+    },
+
+    rules: {
+        "import/no-unresolved": "off",
+        "import/no-useless-path-segments": "off",
+        "no-console": "error",
+    },
+}, {
+    files: ["app/*.tsx", "app/*.tx", " app/*.js", "app/*.jsx"],
+
+    rules: {
+        "import/no-default-export": "off",
+    },
+}, {
+    files: ["**/*.config.js", "**/.eslintrc.js"],
+
+    rules: {
+        "sort-keys": "off",
+        "import/no-commonjs": "off",
+        "unicorn/prefer-module": "off",
+    },
+}];
